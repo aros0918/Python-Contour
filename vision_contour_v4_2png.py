e575ebd2078f739ffe9e822e3c562fbfeb1b9617
aros0918 <naomifujimura918@gmail.com>
2023-11-16 08:05:43 +0200

    Contour problem opencv

--- vision_contour_v4_2png.py
+++ vision_contour_v4_2png.py
@@ -1,269 +1,226 @@
+
 import cv2
+from PIL import Image
 import numpy as np
+from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog, QVBoxLayout, QPushButton
+from collections import deque
+import rembg
+def start():
+    file1 = file_selector1.filename
+    file2 = file_selector2.filename
+    file3 = file_selector3.filename
+    def imagebright(image):
+        average_r = np.mean(image[:, :, 2])
+        average_g = np.mean(image[:, :, 1])
+        average_b = np.mean(image[:, :, 0])
 
-import os
-import time
-import cv2
-import sys
-
-class mycontour:
-    shape = 0
-    color = (0,0,0)
-    center = (0,0)
-    location = []
-
-# Set the directory to watch
-directory = os.path.dirname(os.path.abspath(sys.argv[0]))
-print("next file")
-print(directory)
-# Wait until the file appears in the directory
-while True:
-    if "input.mp4" in os.listdir(directory):
-        break
-    time.sleep(1)
-
-# Open the video file
-cap = cv2.VideoCapture(os.path.join(directory, "input.mp4"))
-# cap = cv2.VideoCapture(os.path.join(directory, "luffy.mp4"))
-
-# Create a "scene" folder if it doesn't exist
-scene_directory = os.path.join(directory, "/scene")
-if not os.path.exists(directory):
-    os.makedirs(directory)
-    print(f"Created directory: {directory}")
-
-# Check if the video file was successfully opened
-if not cap.isOpened():
-    print("Error opening video file")
-
-
-# Define the output video parameters
-output_file = os.path.join(directory, "processed.mp4")
-fps = cap.get(cv2.CAP_PROP_FPS)
-frame_size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-
-# Define the output video writer
-fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-out = cv2.VideoWriter(output_file, fourcc, fps, frame_size)
-
-all_video_array = []
-no_frame = 1
-#cap.set(cv2.CAP_PROP_POS_FRAMES, 26-1)
-# Loop through each frame of the video
-while cap.isOpened():
-    frame_directory = os.path.join(directory+"/scene", f"frame_{no_frame}")
-
-    if not os.path.exists(frame_directory):
-        os.makedirs(frame_directory)
-        # print(f"Created directory: {frame_directory}")
-
-    # Read the current frame
-    ret, frame = cap.read()
-    
-    # Check if the frame was successfully read
-    if not ret:
-        break
-
-    frame = cv2.flip(frame, 0)
-
-    # Convert the image to grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    edges1 = cv2.Canny(gray, 50, 100)
-    black = cv2.threshold(edges1, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-    black_contours, hierarchy = cv2.findContours(black, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    ###---### Use canny edge + preprocessing to get more detail contours
-    blur = cv2.blur(gray,(3,3))
-    edges = cv2.Canny(blur, 100, 200)
-    kernel = np.ones((3, 3), np.uint8)
-    edges  = cv2.morphologyEx(edges , cv2.MORPH_CLOSE,kernel, iterations=1)
-    border_size = 1
-    edges = cv2.copyMakeBorder(
-        edges,
-        top=border_size,
-        bottom=border_size,
-        left=border_size,
-        right=border_size,
-        borderType=cv2.BORDER_CONSTANT,
-        value=[255, 255, 25]
-        )
-    #cv2.imshow('edges', edges)
-    contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # Create a blank image with the same size as the original image
-    output3 = np.zeros_like(frame)
-    # Loop through each contour and find the average color of the pixels inside the contour
-    for contour in contours:
-        # Create a mask for the contour
-        mask = np.zeros_like(gray)
-        cv2.drawContours(mask, [contour], 0, 255, cv2.FILLED)
-
-        # Create a mask for the layer inside the contour
-        layer_mask = np.zeros_like(gray)
-        cv2.drawContours(layer_mask, [contour], 0, 255, thickness=-1)
-
-        # Find the average color of the pixels inside the layer mask
-        average_color = np.mean(frame[np.where(layer_mask == 255)], axis=0)
-        average_color = tuple(map(int, average_color))  # convert to integer tuple
-
-        # Fill the contour with the average color on the output image
-        cv2.fillPoly(output3, [contour], average_color)
-
-
-    #cv2.drawContours(output3, black_contours, -1, 0, 2)
-
-    mask_black = np.zeros_like(gray)
-    cv2.drawContours(mask_black, black_contours, -1, 255, 2)
-    output3[mask_black>0] = (0,0,0)
-
-    ###---### Use canny edge + preprocessing to get more detail contours
-
-    ###---### find contour again from "output3" to get separated contours
-    gray3 = cv2.cvtColor(output3, cv2.COLOR_BGR2GRAY)
-    _,th3 = cv2.threshold(gray3,0,255,cv2.THRESH_BINARY)
-    contours, hierarchy = cv2.findContours(th3, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # Create a blank image with the same size as the original image
-    output4 = np.zeros_like(frame)
-
-    myarray = []
-    count = 1
-    index = 0
-
-    # Loop through each contour and find the average color of the pixels inside the contour
-    for contour in contours:
-        # Create a mask for the contour
-        mask = np.zeros_like(gray)
-        cv2.drawContours(mask, [contour], 0, 255, cv2.FILLED)
-
-        #check if contour is the black lines (from mask_black),if yes, skip it
-        similarity = sum(mask[mask>0] + mask_black[mask>0])/len(mask_black[mask>0])
-        Hit = cv2.bitwise_and(mask[mask>0], mask_black[mask>0] )
-        similarity = np.count_nonzero(Hit)/len(mask_black[mask>0])
-        if similarity > 0.5:
-            continue
-
-        # Create a mask for the layer inside the contour
-        layer_mask = np.zeros_like(gray)
-        cv2.drawContours(layer_mask, [contour], 0, 255, thickness=-1)
-        kernel = np.ones((3, 3), np.uint8)
-        #mask  = cv2.morphologyEx(mask , cv2.MORPH_DILATE,kernel, iterations=1)
-
-        # Find the average color of the pixels inside the layer mask
-        average_color = np.mean(frame[np.where(layer_mask == 255)], axis=0)
-        average_color = tuple(map(int, average_color))  # convert to integer tuple
-
-        # Fill the contour with the average color on the output image
-        #cv2.fillPoly(output4, [contour], average_color)
-
-        M = cv2.moments(contour)
-        if M["m00"] == 0:
-            cX = 0
-            cY = 0
+        average_intensity = (average_r + average_g + average_b) / 3
+        if average_intensity > 110:
+            return True
         else:
-            cX = int(M["m10"] / M["m00"])
-            cY = int(M["m01"] / M["m00"])
+            return False
+    capVideo = cv2.VideoCapture(file1)
+    fps = capVideo.get(cv2.CAP_PROP_FPS)
+    frame_width = int(capVideo.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(capVideo.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    output_video = cv2.VideoWriter('output.mp4', fourcc, fps, (frame_width, frame_height))
+    t = cv2.imread(file2)
+    t = cv2.resize(t, (frame_width, frame_height))
+    background = cv2.imread(file3)
+    background = cv2.resize(background, (frame_width, frame_height))
+    t = cv2.cvtColor(t,cv2.COLOR_BGR2LAB)
+    check = imagebright(t)
 
-        temp = mycontour()
-        temp.shape = count
-        temp.color = average_color
-        temp.center = (cY,cX)
-        temp.location = contour
-        if len(contour)> 10 and len(contour)< 30:
-            index = count
-        myarray.append(temp)
+    def get_mean_and_std(x):
+        x_mean, x_std = cv2.meanStdDev(x)
+        x_mean = np.hstack(np.around(x_mean,2))
+        x_std = np.hstack(np.around(x_std,2))
+        return x_mean, x_std
+        
+    dx = [-1, 0, 1, 0]
+    dy = [0, 1, 0, -1]
 
-        output4[mask>0] = average_color
-        count = count + 1
+    def is_valid_pixel(x, y, rows, cols):
+        return 0 <= x < cols and 0 <= y < rows
 
-    ### make the black lines as same as original
-    ### Tone down the black lines using morphological erosion
-    mask = np.zeros_like(gray)
-    cv2.drawContours(mask, black_contours, -1, 255, 3)
-    kernel = np.ones((3, 3), np.uint8)
-    #mask  = cv2.morphologyEx(mask , cv2.MORPH_ERODE,kernel, iterations=1)
-    #output4[mask>0] = frame[mask>0,:]
+    # Perform BFS to obtain the edge points
+    def bfs(image, start_x, start_y):
+        
+        edge_points = []
 
+        queue = deque()
+        queue.append((start_x, start_y))
+        visited[start_y, start_x] = 255
 
-    #cv2.imshow('mask',mask)
-    #'''
-    black_lines = []
-    black_lines_color = []
-    th_color = 255
-    for w in range(gray.shape[1]):
-        for h in range(gray.shape[0]):
-            if mask[h,w] > 0 and not (frame[h,w,0] > th_color and frame[h,w,1] > th_color and frame[h,w,2] > th_color):
-                output4[h,w,:] = frame[h,w,:] 
-                black_lines.append((h,w))
-                black_lines_color.append(frame[h,w,:])
-
-    temp = mycontour()
-    temp.shape = count
-    temp.color = black_lines_color
-    temp.center = (0,0)
-    temp.location = black_lines
-    myarray.append(temp)
-
-    i = index
-    # print('#contour=',i)
-    # print('shape=',myarray[i].shape)
-    # print('color=',myarray[i].color)
-    # print('lencolor=',len(myarray[i].color))
-    # print('center=',myarray[i].center)
-    # print('location=',myarray[i].location)
-    # print('number of contour=',count)
-
-    height = frame.shape[1]
-    width = frame.shape[0]
-
-    mask = np.zeros((frame.shape),np.uint8)
-    #mask = frame.copy()*0
-    if len(myarray[cnt.shape].color) <= 3:
-        #print('cnt.color=',cnt.color)
-        #cv2.drawContours(mask, cnt.location, -1, cnt.color, cv2.FILLED)
-        cv2.fillPoly(mask, [myarray[cnt.shape].location], cnt.color)
-    else:
-        for idx,loc in enumerate(myarray[cnt.shape].location):
-            #print('color=',cnt.color[idx])
-            #print('loc=',loc)
-            #print('lenloc=',len(loc))
-            if hasattr(loc,"__len__") and loc[1] < height and loc[0] < width:
-                mask[loc[0],loc[1],:] = myarray[cnt.shape].color[idx]
-                #print('loc=',loc[1],',',loc[0])                      
-        #cv2.imshow('mask',mask)
-        #cv2.waitKey(0) 
-    # filename = frame_directory + "contour_" + str(cnt.shape) + ".png"
-    filename = os.path.join(frame_directory, f"contour_{cnt.shape}.png")
-    frame_print = os.path.join(frame_directory, f"front.png")
-
-    cv2.imwrite(filename, mask) 
-    cv2.imwrite(frame_print, frame) 
-        # print(filename)
-        #cv2.imshow('mask',mask)
-        #cv2.waitKey(0)  
-
-    #'''  
-
-    ###---### find contour again from "output3" to get separated contours
-
-
-    #cv2.imshow('Result3', output3)
-    # cv2.imshow('Result4', output4)
-
-    # cv2.imshow('ori', frame)
+        while queue:
+            x, y = queue.popleft()
+            for i in range(4):
+                nx, ny = x + dx[i], y + dy[i]                
+                if is_valid_pixel(nx, ny, rows, cols) and visited[ny, nx] < 100:
+                    visited[ny, nx] = 255
+                    
+                    if image[ny, nx] == 255:
+                        edge_points.append((nx, ny))
+                    else:
+                        queue.append((nx, ny))
+        return edge_points
     
-    all_video_array.append(myarray)
+    while True:
+        result = background.copy()
+        
+        ret, videoFrame = capVideo.read()    
+        
+        if not ret:
+            break
+        video = videoFrame.copy()
+        rows, cols = videoFrame.shape[:2]
+        video = rembg.remove(video)
+        gray = cv2.cvtColor(video, cv2.COLOR_BGR2GRAY)
+        height, width = gray.shape
+        cv2.imshow("je", gray)
+       
+        edges1 = cv2.Canny(gray, 50, 100)
+        black = cv2.threshold(edges1, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+        black_contours, hierarchy = cv2.findContours(black, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        mask_black = np.zeros_like(gray)
+        mask = np.zeros_like(videoFrame)
+        cv2.drawContours(mask_black, black_contours, -1, (255,255,255), 2)
+    
+        visited = np.zeros((rows, cols), dtype=np.uint8)
+        
+        edge_points = bfs(mask_black, 0, 0)
+        for i in range(rows):
+            for j in range(cols):
+                if visited[j][i] == 255:
+                    
+                    videoFrame[j, i] = (0,0,0)
 
-    output = output4
+        s = videoFrame
+        s = cv2.cvtColor(s,cv2.COLOR_BGR2LAB)
+        
+        s_mean, s_std = get_mean_and_std(s)
+        t_mean, t_std = get_mean_and_std(t)
+        height, width, channel = s.shape
 
-    # cv2.waitKey(0)
-    out.write(output)
-    no_frame = no_frame + 1 
-    # print("Directory:", directory)
+        for i in range(0,height):
+            for j in range(0,width):
+                for k in range(0,channel):
+                    x = s[i,j,k]
+                    x = ((x-s_mean[k])*(t_std[k]/s_std[k]))+t_mean[k]
+                    x = round(x)
+                    x = 0 if x<0 else x
+                    x = 255 if x>255 else x
+                    s[i,j,k] = x
 
-    # Wait for the user to press a key to exit
-    #if cv2.waitKey(25) & 0xFF == ord('q'):
-        #break
-# Release the video file and close all windows
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+        s = cv2.cvtColor(s, cv2.COLOR_LAB2BGR)
+        for i in range(s.shape[0]):
+            for j in range(s.shape[1]):
+                r, g, b = s[i, j]
+                if check == False:
+                    if r > 255/2:
+                        r -= 40 * (r - 127)/ 100
+                    else:
+                        r += 40 * (r)/ 100
+                    if g > 255/2:
+                        g -= 40 * (g - 127)/ 100
+                    else:
+                        g += 40 * (g)/ 100
+                    if b > 255/2:
+                        b -= 20 * (b - 127)/ 100
+                    else:
+                        b += 20 * (b)/ 100
+                    s[i, j] = [r, g, b]
+                else:
+                    s[i, j] = [r, g, b]
+
+        for edge_point in edge_points:
+            cv2.circle(mask, edge_point, radius=5, color=(255,255,255), thickness = -1)
+        blurred = cv2.blur(s, (10, 10), dst=None, borderType=cv2.BORDER_DEFAULT)
+        s = np.where(mask != 0, blurred, s)
+
+
+        visiting = np.zeros((rows, cols), dtype=np.uint8)
+        final = np.zeros((rows, cols), dtype=np.uint8)
+        kernel = np.ones((10, 10), np.uint8)
+        visited =  cv2.dilate(visited, kernel, iterations=1)
+        visited =  cv2.erode(visited, kernel, iterations=1)
+        # for i in range(rows):
+        #     for j in range(cols):
+        #         if visited[j][i] == 255:
+        #             for k in range(16):
+        #                 nx, ny = i + ddx[k], j + ddy[k]
+        #                 if is_valid_pixel(nx, ny, rows, cols):
+        #                     visiting[ny, nx] = 255
+        # for i in range(rows):
+        #     for j in range(cols):
+        #         if visiting[j][i] != 255:
+        #             for k in range(10):
+        #                 nx, ny = i + dddx[k], j + dddy[k]
+        #                 if is_valid_pixel(nx, ny, rows, cols):
+        #                     final[ny, nx] = 255
+        # for i in range(rows):
+        #     for j in range(cols):
+        #         if final[j][i] != 255:
+        #             s[j, i] = result[j, i]
+        for i in range(rows):
+            for j in range(cols):
+                if visited[j][i] == 255:
+                    s[j, i] = result[j, i]
+        # s = cv2.fastNlMeansDenoisingColored(s,None,20,20,7,21) 
+        cv2.imshow('result', s)
+
+        output_video.write(s)
+        output_video.write(s)
+        for j in range(2):
+            capVideo.read()
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    capVideo.release()
+    output_video.release()
+    cv2.destroyAllWindows()
+
+class FileSelector(QWidget):
+    def __init__(self, title):
+        super().__init__()
+        self.title = title
+        self.initUI()
+
+    def initUI(self):
+        vbox = QVBoxLayout()
+        self.setLayout(vbox)
+
+        button = QPushButton(self.title)
+        button.clicked.connect(self.showDialog)
+        vbox.addWidget(button)
+
+        self.filename = ""
+
+    def showDialog(self):
+        options = QFileDialog.Options()
+        options = QFileDialog.DontUseNativeDialog
+        filename, _ = QFileDialog.getOpenFileName(self, self.title, "", "All Files (*);;Python Files (*.py)", options=options)
+        if filename:
+            self.filename = filename
+
+if __name__ == '__main__':
+    app = QApplication([])
+    file_selector1 = FileSelector("video file")
+    file_selector2 = FileSelector("png file")
+    file_selector3 = FileSelector("background file")
+    # Create an additional button
+    additional_button = QPushButton("start")
+    additional_button.clicked.connect(start)
+    # Add the button to the layout
+    layout = QVBoxLayout()
+    layout.addWidget(file_selector1)
+    layout.addWidget(file_selector2)
+    layout.addWidget(file_selector3)
+    layout.addWidget(additional_button)
+    
+    # Create a main widget and set the layout
+    main_widget = QWidget()
+    main_widget.setLayout(layout)
+    main_widget.show()
+
+    app.exec_()
